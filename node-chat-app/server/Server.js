cbd5bd2fa5a8903a1cfc45c2e773a10ec6eedc99
vishalk7890 <vishal.sukamble@gmail.com>
2018-04-13 00:39:02 +0530

    done with all

--- node-chat-app/server/Server.js
+++ node-chat-app/server/Server.js
@@ -46,14 +46,22 @@ io.on("connection",(socket) => {
 
     socket.on("createMessage",(message,callback)=>{
         console.log("create email",message)
+        var user=users.getUser(socket.id)
+        if(user && isRealString(message.text)){
+            io.to(user.room).emit("newMessage",generateMessage(user.name,message.text))
+        }
 
-        io.emit("newMessage",generateMessage(message.from,message.text))
+
 
         callback()
     })
 
     socket.on("createLocationMessage",(coords)=>{
-        io.emit("newLocationMessage",generateLocationMessage("admin",coords.lat,coords.lon))
+        var user=users.getUser(socket.id)
+        if(user){
+            io.to(user.room).emit("newLocationMessage",generateLocationMessage(user.name,coords.lat,coords.lon))
+
+        }
     })
 
     socket.on("disconnect",()=>{
